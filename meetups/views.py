488dd8bb2e455b3d243eb07b90f3eea1b1596566
Your Name <you@example.com>
2023-10-27 15:44:45 +0300

    4

--- meetups/views.py
+++ meetups/views.py
@@ -2,4 +2,12 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'meetups/index.html')
+    meetups = [
+        {'title': 'A First Meetup'},
+        {'title': 'A Second Meetup'},
+        {'title': 'A Third Meetup'},
+    ]
+    return render(request, 'meetups/index.html', {
+        'show_meetups': False,
+        'meetups': meetups,
+    })
