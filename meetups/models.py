fc3d102cf1bcbfa122ebf46148e61e530ea7503f
Pavel Penkovskiy <p.penkovskiy@gmail.com>
2023-10-29 22:50:36 +0300

    11

--- meetups/models.py
+++ meetups/models.py
@@ -2,12 +2,20 @@ from django.db import models
 from django.utils.text import slugify
 
 
+class Location(models.Model):
+    name = models.CharField(max_length=200, default='')
+    address = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.name} ({self.address})"
+
+
 class Meetup(models.Model):
     title = models.CharField(max_length=100)
-    location = models.CharField(max_length=100)
     slug = models.SlugField(unique=True, default="", blank=True, null=False, db_index=True)
     description = models.TextField()
     image = models.ImageField(upload_to='images', default='')
+    location = models.ForeignKey(Location, on_delete=models.CASCADE, related_name='posts')
 
     def save(self, *args, **kwargs):
         self.slug = slugify(self.title)
@@ -15,4 +23,3 @@ class Meetup(models.Model):
 
     def __str__(self):
         return f"{self.title} ({self.location})"
-
